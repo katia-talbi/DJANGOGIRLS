5f4ea8ae1d3b48867f1d92614f0c979319dac89e
Katia <katia-talbi@hotmail.com>
2022-10-21 07:29:47 -0700

    My Django Girls app, first commit

---
+++ pythonintro.py
@@ -0,0 +1,10 @@
+array=['hello',23,'SES',40]
+def hi(name):
+    a=1
+    print(name)
+    if a==2 or a==1:
+        print('a is a number between 1 and 2')
+    else:
+        print('CONGRATS 6 will never equal 2')
+for named in array:
+    hi(name)
